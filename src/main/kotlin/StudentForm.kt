31935ce77218b15c6952311c950acf3ae127d682
ayman <aymanmahariq8@gmail.com>
2026-07-15 12:35:06 +0300

    full

--- src/main/kotlin/StudentForm.kt
+++ src/main/kotlin/StudentForm.kt
@@ -1,6 +1,6 @@
 data class StudentForm(
-    var name: String = "",
-    var lastName: String = "",
-    var age: String = "",
-    var university: String = "",
+    var name: String = "ayman",
+    var lastName: String = "almahariq",
+    var age: String = "21",
+    var university: String = "philadelphia",
 )
